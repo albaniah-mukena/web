0d8b81ef8e2f2b3364236f1d3cc6786ff1469567
Husna Ramadan <husna.ramadan@gmial.com>
2023-11-08 16:55:56 +0700

    Add config for redirect the index page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,10 @@
 import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: []
+  integrations: [tailwind()],
+  redirects: {
+    '/': 'https://desty.store/albaniah'
+  }
 });
